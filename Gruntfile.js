b6c26efd69cca7bf357fa5ed91beef4b593dac96
Todd Bashor <todd@thestartersacademy.com>
2012-12-15 01:51:15 -0800

    Generate site.json with walker.js. Generate output with gen.js. Baseline for functionality.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,27 +10,27 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-    
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['test/actual/public/blog/*', 'test/actual/test-result/*', 'test/actual/public/blog/site.json'],
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>',
+			],
+			options: {
+				jshintrc: '.jshintrc',
+			},
+		},
+		
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['test/actual/public/blog/*', 'test/actual/test-result/*', 'test/actual/public/blog/site.json'],
+		},
 
-    // Configuration to be run (and then tested).
-    json: {
-      test:{
+		// Configuration to be run (and then tested).
+		json: {
+			test:{
 				src: ['test/content/blog/**/*.json'],
 				dest: 'test/actual/public/blog/site.json',
 				options: {
@@ -39,42 +39,81 @@ module.exports = function(grunt) {
 					tags: "cool, stuff"
 				}
 			}
-    },
-    genx: {
-      test:{
-				src: ['test/content/blog/posts/*.dust', 'test/content/blog/posts/*.jade'],
+		},
+		genx: {
+			test:{
+				src: ['test/content/blog/posts/*.dust', 'test/content/blog/posts/*.jade', 'test/content/blog/posts/*.md'],
 				dest: 'test/actual/public/blog/',
 				options: {
-					layout: 'test/partials/post.dust',
-					title: "Site Title",
-					author: "Your Name",
-					tags: "awesomeness"
+					context: 'test/actual/site.json'
 				}
 			}
-    },
+		},
+		"genx-file": {
+			test:{
+				src: 'test/content/blog/posts/post1.dust',
+				dest: 'test/actual/public/blog/',
+				options: {
+					"title": "default post name",
+					"author": "Your Name",
+					"tags": "cool, stuff",
+					"layout": "test/partials/post.dust",
+					"blog md": "something at blog level-inside",
+					"metadata": "pages context",
+					"nav": [
+						"page 1",
+						"page 2",
+						"page 3"
+					],
+					"md1": "resources context",
+					"template": "templates/post",
+					"tile": "Hello World",
+					"other": "something else",
+					"name": "post 4 name"
+				}
+			}
+		},
+		Xsite: {
+			test:{
+				src: ['test/content'],
+				dest: 'test/actual/site.json',
+				options: {
+					title: "Site Title",
+					author: "Your Name",
+					tags: "cool, stuff",
+					layout: 'test/partials/post.dust'
+				}
+			}
+		},
+		manifest:{
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+		},
 
-  });
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js'],
+		},
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test-genx', ['clean', 'genx:test', 'nodeunit']);
-  grunt.registerTask('test-json', ['clean', 'json']);
-  grunt.registerTask('run', ['clean','genx:test']);
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test-genx']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+
+	grunt.registerTask('test-genx', ['clean', 'genx:test']);
+	grunt.registerTask('test-genx-file', ['clean', 'genx-file:test']);
+	grunt.registerTask('test-json', ['clean', 'json']);
+	grunt.registerTask('test-site', ['clean', 'site']);
+	grunt.registerTask('run', ['clean','genx:test']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test-genx-file']);
 
 };
--- tasks/genx.js
+++ tasks/genx.js
@@ -10,22 +10,38 @@ module.exports = function(grunt) {
 	
 	"use strict";
 	
-	var htmlGenerator = require('../lib/html'),
-			contextGenerator = require('../lib/context'),
+	var genx = require('../lib/gen'),//html was working previously, rename to genx when done
+			//contextGenerator = require('../lib/flatcontext'),
+			manifestGenerator = require('../lib/manifest'),
 			_ = grunt.util._;
 
 	
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
+	// ==========================================================================
+	// TASKS
+	// ==========================================================================
 
-  grunt.registerMultiTask('genx', 'Generate a static site using any templating engine', function() {
-		htmlGenerator.generateHtmlFiles(this.file.src, this.options({dest: this.file.dest}), this.async());
+	grunt.registerMultiTask('manifest', 'Construct a json object from a directory structure', function() {
+		manifestGenerator.generate(this.file.src, this.file.dest, this.options, this.async());
+	});
+
+	grunt.registerMultiTask('genx-file', 'Render html using any templating engine', function() {
+		grunt.log.ok(JSON.stringify(this.file.src[0]));
+		//var file = grunt.expandFiles(this.file.src);
+		genx.generateFile(this.file.src[0], this.file.dest, this.options(), function(err){
+			if (err) {
+				throw err;
+			}
+		});
+	});
+
+	grunt.registerMultiTask('genx', 'Generate a static site using any templating engine', function() {
+		genx.generateHtmlFiles(this.file.src, this.file.dest, this.options(), this.async());
 	});
 	
-	grunt.registerMultiTask('json', 'Combine json files to form the a context object', function() {
+	/*grunt.registerMultiTask('json', 'Combine json files to form the a context object', function() {
 		contextGenerator.generateSiteContext(this.file.src, this.options({dest: this.file.dest}), this.async());
-	});
+	});*/
 
+	
 };
